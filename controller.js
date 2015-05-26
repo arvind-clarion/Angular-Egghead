af12ede1078f87021b3cbaf45eb9db6506bb2cc4
arvind-clarion <arvind.vyas@clariontechnologies.co.in>
2015-05-26 18:51:57 +0530

    Ep8 Completed

--- controller.js
+++ controller.js
@@ -11,6 +11,20 @@ myApp.controller('MyController', function ($scope) {
      return text.split("").reverse().join();
   };
 })
+.factory('Avengers', function(){
+   var Avengers = {}
+   Avengers.cast =[
+                  {name: "Arvind1", last_name: "Vyas1"},
+                  {name: "Arvind3", last_name: "Vyas2"},
+                  {name: "Arvind4", last_name: "Vyas3"},
+                  {name: "Arvind5", last_name: "Vyas4"},
+                  {name: "Arvind11", last_name: "Vyas5"},
+                  {name: "Arvind7", last_name: "Vyas7"},
+                  {name: "Arvind8", last_name: "Vyas8"},
+                  {name: "Arvind9", last_name: "Vyas9"},
+                  {name: "Arvind12", last_name: "Vyas11"}]
+   return Avengers
+})
 .factory('Data', function(){
    return {message: "I am data from service"}
 })
@@ -27,5 +41,7 @@ myApp.controller('MyController', function ($scope) {
 })
 .controller('ThirdCtrl', function($scope, Data){
   $scope.my_variable = Data;
-
+})
+.controller('AvengersCtrl', function($scope, Avengers){
+  $scope.avengers = Avengers;
 })
