a7d9acf52ef8a203a9b593bb33fb1fff4b1bda5f
arvind-clarion <arvind.vyas@clariontechnologies.co.in>
2015-05-26 12:11:32 +0530

    Completed ep3

--- controller.js
+++ controller.js
@@ -1,5 +1,16 @@
+// Initialize object with angular app
 var myApp = angular.module('myApp', []);
 
+// Bind object with controller
 myApp.controller('MyController', function ($scope) {
   $scope.my_variable = {message: "My First controller testing"}
-});
+})
+.controller('FirstCtrl', function($scope){
+
+})
+.controller('SecondCtrl', function($scope){
+
+})
+.controller('ThirdCtrl', function($scope){
+
+})
